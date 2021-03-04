c6eac3d43234c96d9243982390aeb71f9f10c33b
flower <851192902@qq.com>
2021-03-04 17:34:54 +0800

    Initial commit

---
+++ src/views/Test.vue
@@ -0,0 +1,74 @@
+<!--
+    @Author  :   zhangxiaojian
+    @Time    :   2021/03/03 17:22:00
+ -->
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+                <td>AGE</td>
+            </tr>
+            <tr v-for="item in users">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.age}}</td>
+            </tr>
+        </table>
+        {{msg}}
+    </div>
+</template>
+<script>
+    export default {
+        name: "Test",
+        data() {
+            return {
+                msg: 'hello',
+                users: [
+                    {
+                        id: 1,
+                        name: "zxj",
+                        age: 19
+                    },
+                    {
+                        id: 2,
+                        name: "lc",
+                        age: 18
+                    },
+                    {
+                        id: 3,
+                        name: "ww",
+                        age: 17
+                    }
+                ]
+            }
+        },
+        created() {
+
+            this.axios
+                .get("http://localhost:8888/tests")
+                .then((resp) => {
+                    console.log(resp);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        methods: {
+            // http://localhost:8888/tests/findByPage/' + currentPage
+            tests() {
+                axios.get('http://localhost:8888/tests')
+                    .then(
+                        function (resp) {
+                            console.log(resp);
+                            // _this.tableData = resp.data.data
+                        }
+                    )
+            }
+        },
+    }
+</script>
+<style scoped>
+
+</style>

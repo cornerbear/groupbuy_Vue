b7b11ea9de1692790fc9eff16d36d45c08feb567
flower <851192902@qq.com>
2021-04-15 17:26:40 +0800

    0.6 :tada: 奖励管理完成

--- src/views/manager/ManagerReward.vue
+++ src/views/manager/ManagerReward.vue
@@ -4,17 +4,34 @@
  -->
 <template>
     <div>
+
+        <div class="header">
+            <el-card>
+                <el-button type="primary" plain @click="addDialogVisible=true">
+                    发放奖励
+                </el-button>
+                <el-input placeholder="请输入用户ID" prefix-icon="el-icon-search" v-model="searchModel.userId"
+                    style="width: 200px;margin-left: 5px;margin-right: 5px;"></el-input>
+                <el-button type="primary" plain @click="search">
+                    搜索
+                </el-button>
+            </el-card>
+        </div>
         <el-table :data="tableData" border style="width: 100%" :fit="true">
             <el-table-column prop="userId" label="用户ID">
             </el-table-column>
-            <el-table-column prop="username" label="用户名">
+            <el-table-column prop="rewardName" label="奖励名称">
             </el-table-column>
-            <el-table-column prop="score" label="总评分">
+            <el-table-column prop="rewardDesc" label="详细描述">
+            </el-table-column>
+            <el-table-column prop="createTime" label="创建时间">
+            </el-table-column>
+            <el-table-column prop="actionUserId" label="操作人ID">
             </el-table-column>
             <el-table-column fixed="right" label="操作">
                 <template #default="scope">
-                    <el-button @click="openScoreDetail(scope.row)" type="text" size="small">查看评分记录</el-button>
-                    <el-button @click="openAddScoreLog(scope.row)" type="text" size="small">评分</el-button>
+                    <el-button @click="openUpdateReward(scope.row)" type="text" size="small">修改</el-button>
+                    <el-button @click="deleteReward(scope.row)" type="text" size="small">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -24,47 +41,40 @@
             :page-count="pageCount">
         </el-pagination>
 
-        <!-- 评分模态框 -->
-        <el-dialog title="评分" v-model="addDialogVisible">
-            <el-form :rules="rules" ref="addScoreLogFormRef">
-                <el-form-item label="" prop="action">
-                    <el-radio v-model="addStaffScoreLogModel.action" :label="true">增加</el-radio>
-                    <el-radio v-model="addStaffScoreLogModel.action" :label="false">减少</el-radio>
+        <!-- 发放奖励模态框 -->
+        <el-dialog title="发放奖" v-model="addDialogVisible">
+            <el-form :model="addFormModel" ref="addForm" :rules="rules">
+                <el-form-item label="用户ID" prop="userId">
+                    <el-input v-model="addFormModel.userId" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="变更分数" prop="changeScore">
-                    <el-input v-model="addStaffScoreLogModel.changeScore"></el-input>
+                <el-form-item label="奖励名称" prop="rewardName">
+                    <el-input v-model="addFormModel.rewardName" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="变更原因" prop="reason">
-                    <el-input v-model="addStaffScoreLogModel.reason"></el-input>
+                <el-form-item label="奖励描述" prop="rewardDesc">
+                    <el-input v-model="addFormModel.rewardDesc" autocomplete="off"></el-input>
                 </el-form-item>
             </el-form>
             <template #footer>
                 <span class="dialog-footer">
-                    <el-button @click="closeDialog('addForm')">取 消</el-button>
-                    <el-button type="primary" @click="addScoreLog">确 认</el-button>
+                    <el-button @click="addDialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="addReward('addForm')">确 定</el-button>
                 </span>
             </template>
         </el-dialog>
-
-        <!-- 查看培训模态框 -->
-        <el-dialog title="培训详情" v-model="detailDialogVisible">
-            <el-form :model="dataModel" ref="detailForm" :disable="true">
-                <el-form-item label="培训名称" prop="trainName">
-                    <el-input v-model="dataModel.trainName"></el-input>
+        <!-- 更新奖励模态框 -->
+        <el-dialog title="更新奖励" v-model="updateDialogVisible">
+            <el-form :model="addFormModel" ref="addForm" :rules="rules">
+                <el-form-item label="奖励名称" prop="rewardName">
+                    <el-input v-model="addFormModel.rewardName" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="培训内容" prop="trainContent">
-                    <el-input type="textarea" v-model="dataModel.trainContent"></el-input>
-                </el-form-item>
-                <el-form-item label="发布时间" prop="createTime">
-                    <el-input v-model="dataModel.createTime"></el-input>
-                </el-form-item>
-                <el-form-item label="上传人ID" prop="createUserId">
-                    <el-input v-model="dataModel.createUserId"></el-input>
+                <el-form-item label="奖励描述" prop="rewardDesc">
+                    <el-input v-model="addFormModel.rewardDesc" autocomplete="off"></el-input>
                 </el-form-item>
             </el-form>
             <template #footer>
                 <span class="dialog-footer">
-                    <el-button @click="closeDialog('detailForm')">取 消</el-button>
+                    <el-button @click="updateDialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="updateReward('addForm')">修 改</el-button>
                 </span>
             </template>
         </el-dialog>
@@ -84,36 +94,25 @@
                 pageNo: 1,
                 pageSize: 5,
 
-                // 发布培训
+                // 发放奖励
                 addDialogVisible: false,
-                addStaffScoreLogModel: {
-                    action: true,
-                    changeScore: '',
-                    reason: '',
-                    userId: null,
+                addFormModel: {
+                    userId: '',
+                    rewardName: '',
+                    rewardDesc: '',
                 },
                 rules: {
-                    changeScore: [{
-                        validator: (rule, value, callback) => {
-                            if (!value) {
-                                callback(new Error('分数变更不能为空'))
-                            } else {
-                                if (parseFloat(value) == 0) {
-                                    callback(new Error('分数不能为0'))
-                                } else if (!this.isNumber(value)) {
-                                    callback(new Error('请输入2位以内的小数'))
-                                } else {
-                                    callback()
-                                }
-                            }
-                        },
-                        trigger: 'blur'
-                    }]
+                    userId: [{ required: true, message: '用户ID不能为空', trigger: 'blur' }],
+                    rewardName: [{ required: true, message: '奖励名称不能为空', trigger: 'blur' }],
+                    rewardDesc: [{ required: true, message: '详细描述不能为空', trigger: 'blur' }]
                 },
 
-                // 培训详情
-                detailDialogVisible: false,
+                updateDialogVisible: false,
                 dataModel: null,
+
+                searchModel: {
+                    userId: '',
+                }
 
             }
         },
@@ -127,35 +126,57 @@
                 this.getTableData(this.pageNo, this.pageSize)
             },
             getTableData(pageNo, pageSize) {
-                this.getRequest("/manager/staffTrain/all/" + pageNo + "/" + pageSize).then((resp) => {
+                this.getRequest("/manager/staffReward/all/" + pageNo + "/" + pageSize).then((resp) => {
                     this.tableData = resp.records;
                     this.total = resp.total;
                     this.pageCount = resp.pages;
                 });
             },
-            openScoreDetail(row) {
-                this.getRequest("/manager/staffTrain/" + row.id).then((resp) => {
-                    this.dataModel = resp.data;
-                    this.detailDialogVisible = true;
-                });
-            },
-            openAddScoreLog(row) {
-                console.log(1);
+            openAddReward(row) {
                 this.addDialogVisible = true;
             },
-            addScoreLog() {
-                this.postRequest("/manager/staffScoreLog", this.addStaffScoreLogModel).then((resp) => {
+            addReward() {
+                this.postRequest("/manager/staffReward", this.addFormModel).then((resp) => {
                     console.log(resp);
-                    this.setStaffScore(this.currentUserId);
-                    this.$refs.addScoreLogFormRef.resetFields();
+                    this.addDialogVisible = false;
+                    this.$refs.addForm.resetFields();
+                    this.getTableData(this.pageNo, this.pageSize);
                 });
             },
-            closeDialog(form) {
-                this.addDialogVisible = false;
-                this.detailDialogVisible = false;
-                //清空表单
-                this.$refs[form].resetFields();
-                this.fileList = [];
+            openUpdateReward(row) {
+                this.getRequest("/manager/staffReward/" + row.id).then((resp) => {
+                    console.log(resp);
+                    this.addFormModel = resp.data;
+                    this.updateDialogVisible = true;
+                });
+            },
+            updateReward() {
+                this.putRequest("/manager/staffReward", this.addFormModel).then((resp) => {
+                    console.log(resp);
+                    this.updateDialogVisible = false;
+                    this.$refs.addForm.resetFields();
+                    this.getTableData(this.pageNo, this.pageSize);
+                });
+            },
+            deleteReward(row) {
+                this.$confirm('此操作将删除该奖励, 是否继续?', '警告', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.deleteRequest("/manager/staffReward/" + row.id).then((res) => {
+                        this.getTableData(this.pageNo, this.pageSize);
+                    })
+                }).catch(() => {
+                    this.message.info("已取消删除");
+                });
+            },
+            search() {
+                this.getRequest("/manager/staffReward/all/" + this.pageNo + "/" + this.pageSize + "?userId=" + this.searchModel.userId).then((resp) => {
+                    this.tableData = resp.records;
+                    this.total = resp.total;
+                    this.pageCount = resp.pages;
+                });
             }
         },
         created() {

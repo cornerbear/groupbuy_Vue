3314b01c57d0f40ba670da38c5f9bad5d9a0c69a
flower <851192902@qq.com>
2021-04-21 16:40:19 +0800

    0.9.1 :new: 用户添加商品到购物车

--- src/views/user/UserCart.vue
+++ src/views/user/UserCart.vue
@@ -4,6 +4,15 @@
  -->
 <template>
     <div>
+        <div class="header">
+            <el-card>
+                购物车总价<el-tag type="success">{{cart.totalPrice}}</el-tag>
+                商品数量<el-tag type="success">{{cart.goodsNum}}</el-tag>
+                <el-button type="primary" plain @click="createOrder" style="float:right;">
+                    生成订单
+                </el-button>
+            </el-card>
+        </div>
         <el-table :data="tableData" border style="width: 100%" :fit="true">
             <el-table-column prop="goodsImg" label="图片">
                 <template #default="scope">
@@ -14,11 +23,13 @@
             </el-table-column>
             <el-table-column prop="goodsPrice" label="价格">
             </el-table-column>
-            <el-table-column prop="goodsStock" label="库存">
+            <el-table-column prop="goodsNum" label="数量">
+            </el-table-column>
+            <el-table-column prop="goodsUnit" label="单位">
             </el-table-column>
             <el-table-column fixed="right" label="操作">
                 <template #default="scope">
-                    <el-button @click="addGoodsToCart(scope.row)" type="text" size="small">添加入购物车
+                    <el-button @click="removeGoodsFromCart(scope.row)" type="text" size="small">从购物车删除
                     </el-button>
                 </template>
             </el-table-column>
@@ -28,8 +39,6 @@
             :page-sizes="[5, 10, 20]" :page-size="5" layout="total, sizes, prev, pager, next, jumper" :total="total"
             :page-count="pageCount">
         </el-pagination>
-
-
     </div>
 </template>
 
@@ -38,6 +47,10 @@
     export default {
         data() {
             return {
+                cart:{
+                    totalPrice:0,
+                    goodsNul:0,
+                },
                 total: null,
                 tableData: null,
                 pageCount: null,
@@ -49,13 +62,27 @@
             }
         },
         methods: {
-            addGoodsToCart(row) {
-                this.postRequest("/user/cart/" + row.id).then((resp) => {
-                    if (resp.success) {
-                        this.getTableData(this.pageNo, this.pageSize);
-                    }
-                });
+            createOrder(){
+
             },
+            removeGoodsFromCart(row) {
+                this.$confirm('确认从社区删除该商品?', '警告', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.deleteRequest("/grouper/goods/deleteCommunityGoods/" + row.id).then((resp) => {
+                        if (resp.success) {
+                            this.getTableData(this.pageNo, this.pageSize);
+                        }
+                    });
+                }).catch(() => {
+                    this.message.info("已取消删除");
+                });
+
+            },
+
+
             handleSizeChange(pageSize) {
                 this.pageSize = pageSize;
                 this.getTableData(this.pageNo, this.pageSize)
@@ -66,13 +93,14 @@
             },
             getTableData(pageNo, pageSize) {
 
-                this.getRequest("/user/goods/all/" + pageNo + "/" + pageSize).then((resp) => {
-                        this.tableData = resp.records;
+                this.getRequest("/user/cart/all/" + pageNo + "/" + pageSize).then((resp) => {
+                        this.cart = resp.data;
+                        this.tableData = resp.data.cartItems.records;
                         this.tableData.forEach(element => {
                             element.goodsImg = '/file/image?filePath=' + encodeURI(element.goodsImg);
                         })
-                        this.total = resp.total;
-                        this.pageCount = resp.pages;
+                        this.total = resp.data.cartItems.total;
+                        this.pageCount = resp.data.cartItems.pages;
                 });
             },
         },
@@ -87,5 +115,4 @@
     .header {
         margin-bottom: 5px;
     }
-
 </style>

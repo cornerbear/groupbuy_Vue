37fd78e3a27491a04f888230c4f408ae79aeb1a8
flower <851192902@qq.com>
2021-04-28 18:42:27 +0800

    Revert "0.13.0 :new: 增加登录页判断已登录则跳转功能"
    This reverts commit 3f704a40481c66627d7fe9d11fa947fee9adcb73.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -40,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,19 @@
-let proxyObj = {};
-proxyObj['/ws'] = {
-    ws: true,
-    target: "ws://localhost:8888"
-};
-proxyObj['/'] = {
-    ws: false,
-    target: 'http://localhost:8888',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/': ''
-    }
-}
+// proxyObj['/ws'] = {
+//     ws: true,
+//     target: "ws://localhost:8888"
+// };
 module.exports = {
     devServer: {
         host: 'localhost',
         port: 8080,
-        proxy: proxyObj
+        proxy: {
+            '/' : {
+                target : 'http://localhost:8888',
+                changeOrigin : true,
+                pathRewrite : {
+                    '^/' : ''
+                }
+            }
+        }
     }
 }
